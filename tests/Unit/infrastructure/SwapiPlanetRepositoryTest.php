4e62445affbbae71ee1479b2bb81b0d03b80fafe
Roman <romanslex@gmail.com>
2019-02-24 20:48:43 +0300

    Создал файл теста для SwapiPlanetRepository

---
+++ tests/Unit/infrastructure/SwapiPlanetRepositoryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\infrastructure;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SwapiPlanetRepositoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
