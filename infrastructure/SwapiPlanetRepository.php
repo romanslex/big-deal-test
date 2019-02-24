f320180be1e8fe9f68bca9c98ecd43ef39b583a7
Roman <romanslex@gmail.com>
2019-02-24 20:26:45 +0300

    Делаю запрос по API и получаю планеты в репозитории

--- infrastructure/SwapiPlanetRepository.php
+++ infrastructure/SwapiPlanetRepository.php
@@ -4,18 +4,32 @@ namespace Infrastructure;
 
 use Domain\Planet;
 use Domain\PlanetRepositoryInterface;
+use GuzzleHttp\Client;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 class SwapiPlanetRepository implements PlanetRepositoryInterface
 {
+    /** @var Client */
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => 'http://swapi.co/api/',
+            'timeout'  => 2.0,
+        ]);
+    }
 
     public function get($page): LengthAwarePaginator
     {
-        $items = [
-            new Planet("Earth", "/1"),
-            new Planet("Mars", "/2"),
-        ];
-        $result = new LengthAwarePaginator($items, 50, 10, 1);
+        $response = $this->client->get('planets', ['query' => ['page' => $page]]);
+        $responseBody = json_decode($response->getBody());
+
+        $planets = [];
+        foreach($responseBody->results as $planet)
+            $planets[] = new Planet($planet->name, $planet->url);
+
+        $result = new LengthAwarePaginator($planets, $responseBody->count, 10, $page);
         return $result;
     }
 }
