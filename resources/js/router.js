94a691fcbd4f193208367b3ec19f3778c499d9c4
Roman <romanslex@gmail.com>
2019-02-25 13:10:32 +0300

    Создал компонент для отображения конкретной планеты

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Planets from "./components/Planets.vue";
+import Planet from "./components/Planet.vue";
 
 Vue.use(VueRouter);
 
@@ -8,6 +9,7 @@ const router = new VueRouter({
     mode: 'history',
     routes: [
         { path: '/', component: Planets },
+        { path: '/:id', component: Planet },
     ]
 });
 
