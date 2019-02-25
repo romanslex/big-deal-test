ef4d50df67ccdbb1ad5d644db9cc7a654c2267d9
Roman <romanslex@gmail.com>
2019-02-25 11:16:39 +0300

    Визуальный стиль паджинации

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -3,13 +3,17 @@
         ul#planet-list
             li.planet-list-item(v-for="planet in planets" :key="planet.url")
                 | {{planet.name}}
+        ul#pagination
+            li.pagination-item(v-for="n in lastPage" :class="{'current': n === currentPage}") {{n}}
 </template>
 
 <script>
     export default {
         data(){
             return {
-                planets: []
+                planets: [],
+                lastPage: 1,
+                currentPage: 1,
             }
         },
         mounted() {
@@ -17,7 +21,9 @@
                 .get('/data/planets')
                 .then(response => {
                     console.log(response.data);
-                    this.planets = response.data.data
+                    this.lastPage = response.data.last_page;
+                    this.currentPage = response.data.current_page;
+                    this.planets = response.data.data;
                 })
                 .catch(error => console.log(error));
         }
@@ -33,4 +39,19 @@
         list-style none
         li
             cursor pointer
+    ul#pagination
+        display flex
+        list-style none
+        li
+            display grid
+            height 35px
+            width 35px
+            font-size 12px
+            align-content center
+            justify-content center
+            cursor pointer
+            &.current
+                border solid 1px #3497dc
+                border-radius 50%
+
 </style>
