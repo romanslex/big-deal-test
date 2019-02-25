18778bb0c105c771ee5077c67149b289545441a0
Roman <romanslex@gmail.com>
2019-02-25 15:06:42 +0300

    Поправил тест

--- tests/Unit/infrastructure/SwapiPlanetRepositoryTest.php
+++ tests/Unit/infrastructure/SwapiPlanetRepositoryTest.php
@@ -20,7 +20,7 @@ class SwapiPlanetRepositoryTest extends TestCase
     public function get_200()
     {
         // arrange
-        $planet = new Planet('testName', 'testUrl');
+        $planet = new Planet(1, 'testName', 'https://swapi.co/api/planets/1/');
         $total = 10;
         $currentPage = 1;
         $expected = new LengthAwarePaginator([$planet], $total, 10, $currentPage);
